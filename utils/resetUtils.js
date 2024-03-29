c7ca209c3d063dc1ac845dcd18fea4dc5f9f13f1
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2024-03-29 15:10:48 +0530

    nodemailer added

--- utils/resetUtils.js
+++ utils/resetUtils.js
@@ -6,8 +6,33 @@ exports.generateResetToken = () =>{
 
 // Function to send reset password email
 exports.sendResetPasswordEmail = (email, token) => {
-    // Implement your email sending logic here
-    // For demonstration purposes, you can just log the reset token
-    console.log('Reset token:', token);
-    console.log('Reset password email sent to:', email);
+     // Retrieve email credentials from environment variables
+     const emailUsername = process.env.EMAIL_USERNAME;
+     const emailPassword = process.env.EMAIL_PASSWORD;
+ 
+     // Create a transporter object using SMTP transport
+     const transporter = nodemailer.createTransport({
+         service: 'gmail',
+         auth: {
+             user: emailUsername,
+             pass: emailPassword
+         }
+     });
+ 
+     // Define email options
+     const mailOptions = {
+         from: emailUsername,
+         to: email,
+         subject: 'Password Reset Request',
+         text: `You have requested to reset your password. Please click on the following link to reset your password: http://your-website.com/reset-password/${token}`
+     };
+ 
+     // Send the email
+     transporter.sendMail(mailOptions, (error, info) => {
+         if (error) {
+             console.error('Error sending email:', error);
+         } else {
+             console.log('Email sent:', info.response);
+         }
+     });
 };
