752515a36cbee08f62c75d27378c39c89f43ac79
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2024-03-31 13:59:53 +0530

    updated code

--- index.js
+++ index.js
@@ -36,6 +36,9 @@ app.use('/api/cart', cartRoutes);
 app.get('/',(req,res)=>{
     res.send("Hello");
 })
-app.listen(port, ()=>{
-    console.log(`Server is running on the PORT : http://localhost:${port}`)
-})
+// app.listen(port, ()=>{
+//     console.log(`Server is running on the PORT : http://localhost:${port}`)
+// })
+
+// Export the Express app
+module.exports = app;
