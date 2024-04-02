5175e14797799caa804c7d25412f1469e7db4c89
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2024-04-02 12:53:07 +0530

    updated code

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-const express =  require('express');
+const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-const morgan =  require('morgan');
+const morgan = require('morgan');
+
 const app = express();
 const port = process.env.PORT || 3000;
-dotenv.config()
-// const PORT = 3000;
-const authRoutes = require('./routes/authRoutes'); 
+
+dotenv.config();
+
+// const PORT = 3000; // Removed unused variable
+
+const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes'); // Import order routes
-const dashboardRoutes = require('./routes/dashboardRoutes');    
+const dashboardRoutes = require('./routes/dashboardRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.use(morgan());
 
-app.get('/',(req,res)=>{
-    res.send("Hello");
-})
-mongoose.connect(process.env.DB_CONNECTION_STRING).then(()=>{
-    console.log('Connected to MongoDB')
-}).catch(err=>{
+app.get('/', (req, res) => {
+  res.send("Hello");
+});
+
+mongoose.connect(process.env.DB_CONNECTION_STRING)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
     console.error('Error connecting to MongoDB:', err.message);
     process.exit(1);
-})
+  });
+
 app.use('/api/auth', authRoutes); // Mount authRoutes at /api/auth
 app.use('/api/product', productRoutes); // Mount authRoutes at /api/product
 app.use('/api/order', orderRoutes); // Mount order routes
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/cart', cartRoutes);
-app.listen(port, ()=>{
-    console.log(`Server is running on the PORT : http://localhost:${port}`)
-})
+
+app.listen(port, () => {
+  console.log(`Server is running on the PORT : http://localhost:${port}`);
+});
