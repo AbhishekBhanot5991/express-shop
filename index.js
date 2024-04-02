8966eb9ca57b4d907d40830ac7191cbaf2c7a153
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2024-04-02 12:58:33 +0530

    updated code

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-// const PORT = 3000; // Removed unused variable
-
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes'); // Import order routes
@@ -18,14 +16,14 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(helmet());
-app.use(morgan());
 
 app.get('/', (req, res) => {
   res.send("Hello");
 });
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan());
 
 mongoose.connect(process.env.DB_CONNECTION_STRING)
   .then(() => {
